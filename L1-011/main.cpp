97463c9c5a76fdc5ef992ccb0d2b9f335f917366
codexvn <24852067+Codexvn@users.noreply.github.com>
2019-11-16 14:31:40 +0800

    2019年 11月 16日 星期六 14:31:40 CST

--- L1-011/main.cpp
+++ L1-011/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include<string>
+#include <string>
+
 using namespace std;
+
 int main() {
-    string A,B;
+    string A, B;
     int index;
-    getline(cin,A);
-    getline(cin,B);
+    getline(cin, A);
+    getline(cin, B);
     for (auto i : B) {
-        while ((index=A.find(i))!=-1)
-            A.erase(index,1);
+        while ((index = A.find(i)) != -1)
+            A.erase(index, 1);
     }
-    cout<<A;
- return 0;
+    cout << A;
+    return 0;
 }
