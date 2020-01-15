9fa7c53e7dc43ed3185eacbf806f4780594d97b4
codexvn <24852067+Codexvn@users.noreply.github.com>
2020-01-15 23:22:33 +0800

    2020年 01月 15日 星期三 23:22:33 CST

---
+++ L1-053/L1-053.cpp
@@ -0,0 +1,9 @@
+#include<cstdio>
+using namespace std;
+int main()
+{
+	int A,B;
+	scanf("%d%d", &A, &B);
+	for (int i=A+B;i>0;i--)
+		printf("Wang!");
+}
--- L1-054/L1-054.cpp
+++ L1-054/L1-054.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 int main()
 {
-	char matrix[101][101] = { 0 },replace;
+	char matrix[101][101] = { 0 }, replace;
 	int N;
-	scanf("%c%d%*c",&replace,&N);
+	scanf("%c%d%*c", &replace, &N);
 	for (int i = 0; i < N; i++)
-		cin.getline(matrix[i],101);
+		cin.getline(matrix[i], 101);
 	int j;
-	for (j = 0; j<N/2; j++)
+	for (j = 0; j < N / 2; j++)
 	{
 		for (int k = 0; k < N; k++)
 			if (matrix[j][k] != matrix[N - 1 - j][N - 1 - k])
 				goto break_loop;
 	}
-	break_loop:
-	if (j== N  / 2)
+break_loop:
+	if (j == N / 2)
 		printf("bu yong dao le\n");
-	for (int i = N-1; i >=0; i--)
+	for (int i = N - 1; i >= 0; i--)
 	{
-		for (int j = N-1; j >= 0; j--)
+		for (int j = N - 1; j >= 0; j--)
 		{
-			if(matrix[i][j] == '@')
-				printf("%c",replace);
+			if (matrix[i][j] == '@')
+				printf("%c", replace);
 			else printf("%c", matrix[i][j]);
 
 		}
